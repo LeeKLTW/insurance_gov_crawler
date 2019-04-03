fbcb06f9b09643de5a2a0ea6d3bd154c14b003b4
LeeKLTW <LeeKLTW@gmail.com>
2019-04-03 19:43:14 +0800

    company product list store into sql, change some selector into css style

--- crawler.py
+++ crawler.py
@@ -2,100 +2,80 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
+import time
+from models import conn_sqlite
+import logging
 
-driver = webdriver.PhantomJS('./phantomjs/bin/phantomjs')
+logging.basicConfig(level=logging.INFO, format="[%(levelname)-8s] - %(asctime)s - PID %(process)d - %(message)s")
+
+PREFIX = "http://ins-info.ib.gov.tw/customer/"
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get(PREFIX + 'announceinfo.aspx')
 
 
-driver.get('http://ins-info.ib.gov.tw/customer/announceinfo.aspx')
 ComapnyType = Select(driver.find_element_by_id("ctl00_MainContent_ddlComapnyType"))
+life_insurance = ComapnyType.options[1].text
+ComapnyType.select_by_visible_text(life_insurance)
 
-
-ComapnyType.select_by_visible_text(ComapnyType.options[1].text)
 ComapnyName = Select(driver.find_element_by_id("ctl00_MainContent_ddlComapnyName"))
 
-ComapnyName.select_by_visible_text(ComapnyName.options[1].text)
 
-driver.find_element_by_id('ctl00_MainContent_btnQuery').click()
+def crawl_company_product():
+    # product
+    driver.find_element_by_css_selector(
+        '#contentbox > div.title > table > tbody > tr > td > table > tbody > tr > th > center > input').click()
 
-html_source = driver.page_source
-soup = BeautifulSoup(html_source, 'html.parser')
-prefix = 'http://ins-info.ib.gov.tw/customer/'
-product_url = prefix + soup.select('#CONTENT > table > tbody > tr > td > table:nth-child(12) > tbody > tr > td > a')[0][
-    'href']
-driver.get(product_url)
-driver.find_element_by_id('ctl00_MainContent_btnQuery').click()
-html_source = driver.page_source
-soup = BeautifulSoup(html_source, 'html.parser')
+    html_source = driver.page_source
+    soup = BeautifulSoup(html_source, 'html.parser')
+    candidates = soup.select('tbody > tr:nth-child(1) > td:nth-child(1) span')
+    product_code_list = []
+    product_name_list = []
+    for i in range(len(candidates) - 2):
+        if i % 2 == 0:
+            product_code_list.append(candidates[i].text)
+        elif i % 2 == 1:
+            product_name_list.append(candidates[i].text)
 
-product_code_list = []
-product_name_list = []
-candidates = soup.select('tbody > tr:nth-child(1) > td:nth-child(1) span')
+    conn, cur = conn_sqlite()
+    sql = "INSERT INTO insurance_product (product_id,product_name,company) VALUES (?,?,?);"
 
-for i in range(len(candidates) - 2):
-    if i % 2 == 0:
-        product_code_list.append(candidates[i].text)
-    elif i % 2 == 1:
-        product_name_list.append(candidates[i].text)
+    for id_, name in zip(product_code_list, product_name_list):
+        try:
+            cur.execute(sql, (id_, name, company,))
+            conn.commit()
+        except:
+            print(id_)
 
-product_url = [prefix + a['href'] for a in soup.select('#ctl00_MainContent_dlPaging > tbody > tr > td > a')[1:]][0]
-driver.get(product_url)
-html_source = driver.page_source
-soup = BeautifulSoup(html_source, 'html.parser')
-candidates = soup.select('tbody > tr:nth-child(1) > td:nth-child(1) span')
+    next_page = soup.select(
+        "#contentbox > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td:nth-child(3) a")[
+        0].get('href')
 
-
-for i in range(len(candidates) - 2):
-    if i % 2 == 0:
-        product_code_list.append(candidates[i].text)
-    elif i % 2 == 1:
-        product_name_list.append(candidates[i].text)
+    return next_page
 
 
-driver.find_element_by_css_selector("#func").click()
-driver.find_element_by_css_selector("#func > option:nth-child(3)").click()
+for company in ComapnyName.options[1:]:
+    company = company.text
+    ComapnyName.select_by_visible_text(company)
 
-html_source = driver.page_source
-soup = BeautifulSoup(html_source, 'html.parser')
+    start = time.time()  # for while loop & sleep
+    driver.find_element_by_css_selector(
+        '#contentbox > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td > input').click()
 
-table_title = soup.select("#contentbox > div > table:nth-child(6) > tbody > tr:nth-child(1) > td:nth-child(1)")[0].text
-if '承保' in table_title:
-    coverage_brief = '/'.join([e.text for e in soup.select("#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(1) > pre")])
-    coverage = '/'.join([e.text for e in soup.select("#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(2) > pre")])
+    driver.find_element_by_css_selector(
+        '#CONTENT > table > tbody > tr > td > table:nth-child(12) > tbody > tr > td > a').click()
 
-table_title =soup.select("#contentbox > div > table:nth-child(8) > tbody > tr:nth-child(1) > td:nth-child(1)")[0].text
-if ('不保' in table_title) or ('除外' in table_title):
-    # todo exception v.s. exclusion
-    exception = '/'.join([e.text for e in soup.select("#contentbox > div > table:nth-child(8) > tbody > tr > td:nth-child(2) > pre")])
-
-driver.get(product_url)
-
-
-
-
-driver.find_element_by_id("ctl00_MainContent_DataGridSearchResults > tbody > tr:nth-child(1) > td:nth-child(3)")
-product_info = driver.find_element_by_css_selector("ctl00_MainContent_DataGridSearchResults > tbody > tr:nth-child > td:nth-child")
-product_info = driver.find_element_by_id("func")
-product_info.__dict__
-dir(product_info)
-soup.select("tbody tr  td")
-soup.select("#ctl00_MainContent_DataGridSearchResults > tbody > tr > td")
-"#ctl00_MainContent_DataGridSearchResults > tbody > tr"
-
-
-
-"#func"
-def crawl():
-    pass
-
-
-def store():
-    pass
+    next_page = crawl_company_product()
+    while next_page:
+        next_page = PREFIX + next_page
+        driver.get(next_page)
+        next_page = crawl_company_product()
+    else:
+        break
 
 
 def main():
-    crawl()
-    store()
-
+    pass
 
 if __name__ == '__main__':
     main()
