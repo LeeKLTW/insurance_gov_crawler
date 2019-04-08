13d123c5a06f99bfee33dd2172c0ba3e6721863a
LeeKLTW <LeeKLTW@gmail.com>
2019-04-08 18:58:26 +0800

    recursive with call back crawl product detail

--- crawler.py
+++ crawler.py
@@ -2,7 +2,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
-import time
 from models import conn_sqlite
 import logging
 
@@ -11,61 +10,22 @@ logging.basicConfig(level=logging.INFO, format="[%(levelname)-8s] - %(asctime)s 
 PREFIX = "http://ins-info.ib.gov.tw/customer/"
 
 
-def crawl_products(company):
-    # product
-    driver.find_element_by_css_selector(
-        '#contentbox > div.title > table > tbody > tr > td > table > tbody > tr > th > center > input').click()
-
-    html_source = driver.page_source
-    soup = BeautifulSoup(html_source, 'html.parser')
-    candidates = soup.select('tbody > tr:nth-child(1) > td:nth-child(1) span')
-    product_code_list = []
-    product_name_list = []
-
-    for i in range(len(candidates) - 2):
-        if i % 2 == 0:
-            product_code_list.append(candidates[i].text)
-        elif i % 2 == 1:
-            product_name_list.append(candidates[i].text)
-
-    conn, cur = conn_sqlite()
-
-    id_, name, company = 'testid', 'testname', 'testcompany'
-
-    sql = """
-    IF NOT EXISTS (SELECT * FROM insurance_product WHERE product_id=?)
-        BEGIN
-            INSERT INTO insurance_product (product_id,product_name,company) VALUES (?,?,?)
-        End
-    """
-    # sql = "INSERT INTO insurance_product (product_id,product_name,company) VALUES (?,?,?);"
-    cur.execute(sql, (id_,id_, name, company,))
-    conn.commit()
-
-    for id_, name in zip(product_code_list, product_name_list):
-        try:
-            # print(sql)
-            # cur.execute(sql, (id_,id_, name, company,))
-            cur.execute(sql, (id_, name, company,))
-            conn.commit()
-        except:
-            print(id_)
+class InsuranceProduct:
+    def __int__(self, product_id, product_name, company, coverage_brief=None, coverage=None, exception=None,
+                exception_brief=None, dividend=None, rejection=None, claim_app_doc=None):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.company = company
+        self.coverage_brief = coverage_brief
+        self.coverage = coverage
+        self.exception = exception
+        self.exception_brief = exception_brief
+        self.dividend = dividend
+        self.rejection = rejection
+        self.claim_app_doc = claim_app_doc
 
 
-    next_page = soup.select(
-        "#contentbox > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td:nth-child(3) a")[
-        0].get('href')
-
-    if next_page is None:
-        return
-    else:
-        next_page = PREFIX + next_page
-        driver.get(next_page)
-        crawl_products(company)
-    return
-
-
-def clist_2_plist(company):
+def clist_to_ppage(company):
     driver.get(PREFIX + 'announceinfo.aspx')
 
     ComapnyType = Select(driver.find_element_by_id("ctl00_MainContent_ddlComapnyType"))
@@ -77,6 +37,116 @@ def clist_2_plist(company):
         '#contentbox > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td > input').click()
     driver.find_element_by_css_selector(
         '#CONTENT > table > tbody > tr > td > table:nth-child(12) > tbody > tr > td > a').click()
+    return
+
+
+def iter_products(company):
+    # product
+    driver.find_element_by_css_selector(
+        '#contentbox > div.title > table > tbody > tr > td > table > tbody > tr > th > center > input').click()
+
+    html_source = driver.page_source
+    soup = BeautifulSoup(html_source, 'html.parser')
+    candidates = soup.select('tbody > tr > td:nth-child(1) span')
+    product_page_url = driver.current_url
+
+    def _write_product_detail(i):
+        p = InsuranceProduct(product_id=candidates[i - 1], product_name=candidates[i], company=company)
+
+        idx = int(i // 2)
+        idx_html = f"#ctl00_MainContent_DataGridSearchResults > tbody > tr:nth-child({idx}) > td:nth-child(3) >select"
+        term_opt = Select(driver.find_element_by_css_selector(idx_html))
+        coverage_visible = [i.text for i in term_opt.options if '承保' in i.text][0]
+        dividend_visible = [i.text for i in term_opt.options if '紅利' in i.text][0]
+        rejection_visible = [i.text for i in term_opt.options if '職業' in i.text][0]
+        claim_app_doc_visible = [i.text for i in term_opt.options if '文件' in i.text][0]
+
+
+        def _call_back():
+            driver.get(product_page_url)
+            driver.find_element_by_css_selector(
+                '#contentbox > div.title > table > tbody > tr > td > table > tbody > tr > th > center > input').click()
+            term_opt = Select(driver.find_element_by_css_selector(idx_html))
+            return term_opt
+
+        term_opt = _call_back()
+        term_opt.select_by_visible_text(coverage_visible)
+
+        html_source = driver.page_source
+        soup = BeautifulSoup(html_source, 'html.parser')
+
+        coverage_li = [i.text.replace(' ', '').replace('\n', '') for i in
+                       soup.select("#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(2)")][1:]
+        coverage_b_li = [i.text.replace(' ', '').replace('\n', '') for i in
+                         soup.select("#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(1)")][1:]
+        exception_li = [i.text.replace(' ', '').replace('\n', '') for i in
+                        soup.select("#contentbox > div > table:nth-child(8) > tbody > tr > td:nth-child(2)")][1:]
+        exception_b_li = [i.text.replace(' ', '').replace('\n', '') for i in
+                          soup.select("#contentbox > div > table:nth-child(8) > tbody > tr > td:nth-child(1)")][1:]
+
+        p.coverage = '/'.join(coverage_li)
+        p.coverage_brief = '/'.join(coverage_b_li)
+        p.exception = '/'.join(exception_li)
+        p.exception_brief = '/'.join(exception_b_li)
+
+
+        term_opt = _call_back()
+        term_opt.select_by_visible_text(dividend_visible)
+        html_source = driver.page_source
+        soup = BeautifulSoup(html_source, 'html.parser')
+        dividend_li = [i.text.replace(' ', '').replace('\n', '') for i in
+                        soup.select("#contentbox > table > tbody > tr > td:nth-child(2)")][3:]
+        p.dividend = '/'.join(dividend_li)
+
+
+        term_opt = _call_back()
+        term_opt.select_by_visible_text(rejection_visible)
+        html_source = driver.page_source
+        soup = BeautifulSoup(html_source, 'html.parser')
+        li1 = [i.text.replace(' ', '').replace('\n', '') for i in soup.select("#contentbox > div > table > tbody > tr > td:nth-child(1)")][3:]
+        li2 = [i.text.replace(' ', '').replace('\n', '') for i in soup.select("#contentbox > div > table > tbody > tr > td:nth-child(2)")][3:]
+        rejection_li = []
+        for i,j in zip(li1,li2):
+            rejection_li.append(str(i)+": "+str(j))
+        p.rejection = '/'.join(rejection_li)
+
+
+        term_opt = _call_back()
+        term_opt.select_by_visible_text(claim_app_doc_visible)
+        html_source = driver.page_source
+        soup = BeautifulSoup(html_source, 'html.parser')
+        li1 = [i.text.replace(' ', '').replace('\n', '') for i in soup.select("#contentbox > div > table > tbody > tr > td:nth-child(1)")][3:]
+        li2 = [i.text.replace(' ', '').replace('\n', '') for i in soup.select("#contentbox > div > table > tbody > tr > td:nth-child(2)")][3:]
+        claim_app_doc_li = []
+        for i,j in zip(li1,li2):
+            claim_app_doc_li.append(str(i)+": "+str(j))
+        p.claim_app_doc = '/'.join(rejection_li)
+
+
+        sql = """
+        INSERT INTO insurance_product (product_id,product_name,company)
+        SELECT ?,?,?
+        WHERE NOT EXISTS(SELECT * FROM insurance_product WHERE product_id=?);
+        """
+
+        conn, cur = conn_sqlite()
+        cur.execute(sql, (p.product_id, p.product_name, p.company, p.product_id))
+        conn.commit()
+
+    for i in range(2, len(candidates) - 2, 2):
+        _write_product_detail(i)
+
+
+    next_page = soup.select(
+        "#contentbox > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td:nth-child(3) a")[
+        0].get('href')
+
+    if next_page is None:
+        return
+    else:
+        next_page = PREFIX + next_page
+        driver.get(next_page)
+        iter_products(company)
     return
 
 
@@ -94,8 +164,13 @@ def main():
     companys_list = [c.text for c in ComapnyName.options[1:]]
 
     for company in companys_list:
-        clist_2_plist(company)
-        crawl_products(company)
+        try:
+            clist_to_ppage(company)
+            iter_products(company)
+        except:
+            print('Cannot get', company)  # e.g. '國華人壽保險股份有限公司'
+
+    driver.close()
 
 
 if __name__ == '__main__':
