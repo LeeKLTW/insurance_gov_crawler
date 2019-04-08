b5c694f92dea525a2ba887075b1df7be7a63b7fa
LeeKLTW <LeeKLTW@gmail.com>
2019-04-08 18:58:58 +0800

    test

--- models.py
+++ models.py
@@ -20,4 +20,56 @@ def _create_table_insurance_product(cur):
     claim_app_doc  TEXT,
     PRIMARY KEY (product_id));""")
     return
-
+#
+#
+# conn, cur = conn_sqlite()
+# sql = """CREATE TABLE trip(
+# trip_id INT NOT NULL AUTO_INCREMENT,
+# user VARCHAR(64) NOT NULL,
+# country VARCHAR(64),
+# PRIMARY KEY (trip_id));"""
+# cur.execute(sql)
+# sql = "DROP TABLE trip;"
+# cur.execute(sql)
+# conn.commit()
+#
+# trip_li = [('tony','US'),('tony','HK'),('kelly','US')]
+#
+# for t in trip_li:
+#     sql = """INSERT INTO `trip`(`user`,`country`) VALUES(?,?)"""
+#     cur.execute(sql,(t[0],t[1]))
+#     conn.commit()
+#
+#
+#
+#
+#
+# sql = """
+# IF NOT EXISTS (SELECT * FROM trip WHERE user='shelly')
+# BEGIN
+#     INSERT INTO `trip`(`user`,`country`) VALUES('shelly','UK')
+# END
+# """
+#
+# sql = """
+# INSERT OR IGNORE INTO `trip`(`user`,`country`) VALUES ('shelly','UK')
+# UPDATE `trip` SET country='UK' WHERE user = 'shelly';
+# """
+#
+# sql = """
+# INSERT INTO `trip`(`user`,`country`)
+# SELECT 'shelly','UK'
+# WHERE NOT EXISTS(SELECT * FROM `trip` WHERE `user`='shelly');
+# """
+#
+# cur.execute(sql)
+#
+# conn.commit()
+#
+#
+#
+#
+# cur.execute("SELECT * FROM `trip`;")
+# cur.execute("""SELECT * FROM `trip` WHERE `user` = "tony" AND `country` = "US" LIMIT 1""")
+# res = cur.fetchall()
+# print(res)
