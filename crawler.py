a0acd27bd2bffe88f9d9e90c813e748fe843e1a7
LeeKLTW <LeeKLTW@gmail.com>
2019-04-09 17:09:43 +0800

    typo: __int__ => __init__ & fix pylint line too long 120 => 100

--- crawler.py
+++ crawler.py
@@ -5,14 +5,16 @@ from bs4 import BeautifulSoup
 from models import conn_mysql
 import logging
 
-logging.basicConfig(level=logging.INFO, format="[%(levelname)-8s] - %(asctime)s - PID %(process)d - %(message)s")
+logging.basicConfig(level=logging.INFO,
+                    format="[%(levelname)-8s] - %(asctime)s - PID %(process)d - %(message)s")
 
 PREFIX = "http://ins-info.ib.gov.tw/customer/"
 
 
 class InsuranceProduct:
-    def __int__(self, product_id, product_name, company, coverage_brief=None, coverage=None, exception=None,
-                exception_brief=None, dividend=None, rejection=None, claim_app_doc=None):
+    def __init__(self, product_id, product_name, company, coverage_brief=None, coverage=None,
+                 exception=None,
+                 exception_brief=None, dividend=None, rejection=None, claim_app_doc=None):
         self.product_id = product_id
         self.product_name = product_name
         self.company = company
@@ -51,7 +53,8 @@ def iter_products(company):
     product_page_url = driver.current_url
 
     def _write_product_detail(i):
-        p = InsuranceProduct(product_id=candidates[i - 1], product_name=candidates[i], company=company)
+        p = InsuranceProduct(product_id=candidates[i - 1], product_name=candidates[i],
+                             company=company)
 
         idx = int(i // 2)
         idx_html = f"#ctl00_MainContent_DataGridSearchResults > tbody > tr:nth-child({idx}) > td:nth-child(3) >select"
@@ -75,13 +78,25 @@ def iter_products(company):
         soup = BeautifulSoup(html_source, 'html.parser')
 
         coverage_li = [i.text.replace(' ', '').replace('\n', '') for i in
-                       soup.select("#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(2)")][1:]
+                       soup.select(
+                           "#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(2)")][
+                      1:]
         coverage_b_li = [i.text.replace(' ', '').replace('\n', '') for i in
-                         soup.select("#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(1)")][1:]
+                         soup.select(
+                             "#contentbox > div > table:nth-child(6) > tbody > tr > td:nth-child(1)")][
+                        1:]
         exception_li = [i.text.replace(' ', '').replace('\n', '') for i in
-                        soup.select("#contentbox > div > table:nth-child(8) > tbody > tr > td:nth-child(2)")][1:]
+                        soup.select(
+                            "#contentbox > div > table:nth-child(8) > tbody > tr > td:nth-child(2)")][
+                       1:]
         exception_b_li = [i.text.replace(' ', '').replace('\n', '') for i in
-                          soup.select("#contentbox > div > table:nth-child(8) > tbody > tr > td:nth-child(1)")][1:]
+                          soup.select(
+                              "#contentbox > div > table:nth-child(8) > tbody > tr > td:nth-child(1)")][
+                         1:]
+
+        p = InsuranceProduct(product_id='202391RZ9DGE021A11Z10000007',
+                             product_name='台灣人壽一年期二至十一級失能健康保險附約',
+                             company=company)
 
         p.coverage = '/'.join(coverage_li)
         p.coverage_brief = '/'.join(coverage_b_li)
@@ -123,15 +138,17 @@ def iter_products(company):
         p.claim_app_doc = '/'.join(rejection_li)
 
         sql = """
-        INSERT INTO insurance_product (product_id,product_name,company,coverage_brief,coverage,exception,exception_brief,dividend,rejection,claim_app_doc)
+        INSERT INTO insurance_product 
+        (product_id,product_name,company,coverage_brief,coverage,exception,exception_brief,dividend,rejection,claim_app_doc)
         SELECT ?,?,?,?,?,?,?,?,?,?
         WHERE NOT EXISTS(SELECT * FROM insurance_product WHERE product_id=?);
         """
 
         conn, cur = conn_mysql()
         cur.execute(sql, (
-        p.product_id, p.product_name, p.company, p.coverage_brief, p.coverage, p.exception, p.exception_brief,
-        p.dividend, p.rejection, p.claim_app_doc, p.product_id))
+            p.product_id, p.product_name, p.company, p.coverage_brief, p.coverage, p.exception,
+            p.exception_brief,
+            p.dividend, p.rejection, p.claim_app_doc, p.product_id))
         conn.commit()
         conn.close()
 
