465747606e3512784c398ccab2b8700c8a441a7f
LeeKLTW <LeeKLTW@gmail.com>
2019-04-09 16:52:08 +0800

    finish def conn_mysql & use acc.json

--- models.py
+++ models.py
@@ -1,10 +1,23 @@
 # encoding: utf-8
 import sqlite3
 import pymysql
-
+import json
 
 def conn_sqlite():
     conn = sqlite3.connect('InsuranceProduct.db')
+    cur = conn.cursor()
+    return conn, cur
+
+
+def conn_mysql():
+    s = open('acc.json', 'r').readlines()[0]
+    s = json.loads(s)
+
+    conn = pymysql.Connection(**s['database'],charset='utf8')  # utf8 to correct encoding
+    cur = conn.cursor()
+
+    database = s['database']['database']
+    cur.execute(f'USE {database};')
     cur = conn.cursor()
     return conn, cur
 
@@ -22,4 +35,3 @@ def _create_table_insurance_product(cur):
     claim_app_doc  TEXT,
     PRIMARY KEY (product_id));""")
     return
-
